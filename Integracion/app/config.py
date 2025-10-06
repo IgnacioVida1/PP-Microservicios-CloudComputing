b8bbc6b3f4476ae11ebfe89563402cc1121e1233
GabrielFrisancho <gabriel.frisancho@utec.edu.pe>
2025-10-05 22:43:07 -0500

    fix ports

--- Integracion/app/config.py
+++ Integracion/app/config.py
@@ -5,12 +5,12 @@ load_dotenv()
 
 class Config:
     # 🔄 URLs actualizadas de los microservicios (usar variables de entorno)
-    MS1_PRODUCTOS_URL = os.getenv("MS1_PRODUCTOS_URL", "http://api_productos:8001")  # ProductosYAlmacen
-    MS2_LOGISTICA_URL = os.getenv("MS2_LOGISTICA_URL", "http://api_logistica:8002")   # Micrologistica
-    MS3_PEDIDOS_URL = os.getenv("MS3_PEDIDOS_URL", "http://api_pedidos:8003")       # PedidosYClientes
+    MS1_PRODUCTOS_URL = os.getenv("MS1_PRODUCTOS_URL", "http://api_productos:8000")  # ProductosYAlmacen
+    MS2_LOGISTICA_URL = os.getenv("MS2_LOGISTICA_URL", "http://api_logistica:8001")   # Micrologistica
+    MS3_PEDIDOS_URL = os.getenv("MS3_PEDIDOS_URL", "http://api_pedidos:8002")       # PedidosYClientes
     
     # Configuración del servidor (se mantiene igual)
-    PORT = int(os.getenv("PORT", 8004))
+    PORT = int(os.getenv("PORT", 8003))
     HOST = os.getenv("HOST", "0.0.0.0")
 
 config = Config()
